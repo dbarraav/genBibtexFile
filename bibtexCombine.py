ce5189e6909454b1deacc9b2752a83ff8c52319e
Diego Barra Avila <dbarraa98@gmail.com>
2024-04-19 17:55:12 -0500

    Combine and remove copies properly

--- bibtexCombine.py
+++ bibtexCombine.py
@@ -25,28 +25,45 @@ bibtex_dict = defaultdict(list)
 pattern = re.compile(r'@(\w+){([^,]+),([^@]+)}', re.DOTALL)
 
 # Iterate over each file in the directory
-for filename in os.listdir(directory):
-    if filename.endswith('.txt') and "bibtexEntries" in filename:
-        with open(os.path.join(directory, filename), 'r') as file:
-            content = file.read()
-            # Find all bibtex entries in the file
-            matches = pattern.findall(content)
-            for match in matches:
-                # Extract the citekey and the entry
-                entry_type, citekey, entry = match
-                # Store the entry in the dictionary
-                # bibtex_dict[citekey].append(f'@{entry_type}{{{citekey},\n{entry}}}')
-                bibtex_dict[citekey].append(f'@{entry_type}{{{citekey},\n{entry.strip()}}}')
+# for filename in os.listdir(directory):
+#     if filename.endswith('.txt') and "bibtexEntries" in filename:
+
+for root, dirs, files in os.walk(directory):
+    for filename in files:
+        if filename.endswith('.txt') and "bibtexEntries" in filename:
+            with open(os.path.join(root, filename), 'r') as file:
+                content = file.read()
+                # Find all bibtex entries in the file
+                matches = pattern.findall(content)
+                for match in matches:
+                    # Extract the citekey and the entry
+                    entry_type, citekey, entry = match
+                    # Store the entry in the dictionary
+                    # bibtex_dict[citekey].append(f'@{entry_type}{{{citekey},\n{entry}}}')
+                    bibtex_dict[citekey].append(f'@{entry_type}{{{citekey},\n{entry.strip()}}}')
                 
 
 # Dictionary to store the final bibtex entries
 final_bibtex_dict = {}
+
+# New dictionary to store entries by title
+title_dict = defaultdict(list)
 
 # Iterate over the bibtex entries
 for citekey, entries in bibtex_dict.items():
     # If there are multiple entries with the same citekey
     if len(entries) > 1:
         for i, entry in enumerate(entries):
+            title = re.search(r'title\s*=\s*{([^}]+)}', entry, re.IGNORECASE)
+
+            if title:
+                title = title.group(1)
+                # If title already exists in title_dict
+                if title in title_dict:
+                    continue
+                else:
+                    # Add title in title_dict
+                    title_dict[title].append(entry)
             # Add a letter to the citekey
             new_citekey = citekey + chr(97 + i)
             # Replace the old citekey with the new citekey in the entry
@@ -66,7 +83,7 @@ with open(bibtexFilePath, 'w') as file:
     for citekey, entry in sorted_entries:
         file.write(entry + '\n\n')
 
-shutil.copy(bibtexFilePath, os.path.join(directory, 'final_bibtex_entries.bib'))
+# shutil.copy(bibtexFilePath, os.path.join(directory, 'final_bibtex_entries.bib'))
 
 # def copy_and_change_ext(src, dst, new_extension):
 #     # Get the base name of the source file without the extension
